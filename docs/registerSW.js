18f8d3fd09b5bf269cab0e3077ffc977fa941658
bowoXymoan01 <andrewibowocahjtb@gmail.com>
2025-05-10 19:48:55 +0700

    deploy

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/StoryApp/sw.js', { scope: '/StoryApp/' })})}
